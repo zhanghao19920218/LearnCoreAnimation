1911a8c03875c65cd3e3d7f8ebe6dd528ae17f3f
zhanghao19920218 <2420615378@qq.com>
2021-01-25 11:23:22 +0800

    1. 完成对图层的了解 2. 图层的创建 3. 图层CALayer和UIView区别的了解 4. UIView封装的原因

--- LearnCoreAnimation/ViewController.swift
+++ LearnCoreAnimation/ViewController.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var v: UIView = {
+        let v = UIView()
+        v.backgroundColor = .white
+        v.frame = CGRect(x: UIScreen.main.bounds.size.width / 2 - 100,
+                         y: UIScreen.main.bounds.size.height / 2 - 100,
+                         width: 200,
+                         height: 200)
+        return v
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(v)
+        
+        let blueLayer = CALayer()
+        blueLayer.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+        blueLayer.backgroundColor = UIColor.blue.cgColor
+        
+        v.layer.addSublayer(blueLayer)
     }
 
 
